db0d356a2f0b87a745f4ab6a65322e4e9863f7c2
nizamsardre <63792425+nizamsardre@users.noreply.github.com>
2022-09-16 21:43:09 +0600

    immutable

---
+++ string.java
@@ -0,0 +1,10 @@
+class Demo
+{ 
+    public static void main(String args[])
+    { 
+        String s="Sachin"; 
+        s.concat(" Tendulkar");    //concat() method appends the string at the end
+        //s=s.concat(" Tendulkar");
+        System.out.println(s);    //will print Sachin because strings are immutable objects 
+    } 
+}
