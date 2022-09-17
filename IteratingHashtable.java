05094721b289fb4f76dde1e45b72de7470512620
nizamsardre <63792425+nizamsardre@users.noreply.github.com>
2022-09-17 13:45:38 +0600

    not null

---
+++ IteratingHashtable.java
@@ -0,0 +1,23 @@
+// Java program to illustrate
+// traversal of Hashtable
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class IteratingHashtable {
+	public static void main(String[] args)
+	{
+		// Create an instance of Hashtable
+		Hashtable<String, Integer> ht = new Hashtable<>();
+
+		// Adding elements using put method
+		ht.put("vishal", 10);
+		ht.put("sachin", 30);
+		ht.put("vaibhav", 20);
+	
+		// Iterating using enhanced for loop
+		for (Map.Entry<String, Integer> e : ht.entrySet())
+			System.out.println(e.getKey() + " "
+							+ e.getValue());
+	}
+}
